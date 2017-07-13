9ecd24d1c683ca26a385731ea3b50ef452b589f0
Ana Luz Acevedo-Cabrera <analuzacevedo-cabrera@Anas-MacBook.local>
2017-07-12 20:32:12 -0700

    updated to include my answer

--- abstract-functions.js
+++ abstract-functions.js
@@ -1,34 +1,70 @@
 // Clean up the code below with a single function called `personalIntroduction`
 // The new code should have the exact same functionality and output.
 
-var name = "Bob";
-var height = "72";
-var favoriteColor = "red";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
+//My answer below:
 
-var name = "Samantha";
-var height = "65";
-var favoriteColor = "orange";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
+var ledata=[
+{
+  name: "Bob",
+  height: "72",
+  favoriteColor: "red"
+},
+{
+  name: "Samantha",
+  height: "65",
+  favoriteColor: "orange"
+},
+{
+  name: "Tamera",
+  height: "63",
+  favoriteColor: "blue"
+}
+];
 
-var name = "Christopher";
-var height = "69";
-var favoriteColor = "green";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
+function personalIntroduction(ledata){
+var stringArray=[];
+for (i=0; i<ledata.length; i++){
+  var string = "My name is " + ledata[i].name;
+  string += ", and my height is " + ledata[i].height;
+  string += ", and my favorite color is " + ledata[i].favoriteColor;
+  stringArray.push(string);
+}
+return stringArray;
+}
 
-var name = "Tamera";
-var height = "63";
-var favoriteColor = "blue";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
+personalIntroduction(ledata);
+
+
+// ORIGINAL CODE BELOW:
+//
+// var name = "Bob";
+// var height = "72";
+// var favoriteColor = "red";
+// var string = "My name is " + name;
+// string += ", and my height is " + height;
+// string += ", and my favorite color is " + favoriteColor;
+// console.log(string);
+//
+// var name = "Samantha";
+// var height = "65";
+// var favoriteColor = "orange";
+// var string = "My name is " + name;
+// string += ", and my height is " + height;
+// string += ", and my favorite color is " + favoriteColor;
+// console.log(string);
+//
+// var name = "Christopher";
+// var height = "69";
+// var favoriteColor = "green";
+// var string = "My name is " + name;
+// string += ", and my height is " + height;
+// string += ", and my favorite color is " + favoriteColor;
+// console.log(string);
+//
+// var name = "Tamera";
+// var height = "63";
+// var favoriteColor = "blue";
+// var string = "My name is " + name;
+// string += ", and my height is " + height;
+// string += ", and my favorite color is " + favoriteColor;
+// console.log(string);
